b84b78b728ca3d10c441ef84dd13aacaae465263
Geoffroy Givry <geoff.givry@gmail.com>
2019-01-29 23:16:42 +0000

    refactored code for topbar

--- src/app/App.js
+++ src/app/App.js
@@ -3,9 +3,13 @@ import Topbar from "../components/Topbar";
 
 const Asgard = () => {
   return (
-    <div>
-      <Topbar />
-      <div>Hello World!</div>
+    <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+      <header class="mdl-layout__header">
+        <Topbar />
+      </header>
+      <main class="mdl-layout__content">
+        <div class="page-content">Hello World!</div>
+      </main>
     </div>
   );
 };
--- src/components/Topbar.js
+++ src/components/Topbar.js
@@ -1,73 +1,40 @@
 import React from "react";
 
 class Topbar extends React.Component {
+  state = { content: null };
   render() {
     return (
-      <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
-        <header class="mdl-layout__header">
-          <div class="mdl-layout__header-row">
-            <span class="mdl-layout-title">Title</span>
-            <div class="mdl-layout-spacer" />
-            <nav class="mdl-navigation mdl-layout--large-screen-only">
-              <a
-                class="mdl-navigation__link"
-                href="https://github.com/geoffroygivry/"
-              >
-                My github
-              </a>
-              <a
-                class="mdl-navigation__link"
-                href="https://github.com/geoffroygivry/CyclopsVFX-Unity"
-              >
-                My github Cyclops Unity
-              </a>
-              <a
-                class="mdl-navigation__link"
-                href="https://github.com/geoffroygivry/CyclopsVFX-Arges"
-              >
-                My github Cyclops Arges
-              </a>
-              <a
-                class="mdl-navigation__link"
-                href="https://github.com/geoffroygivry/CyclopsVFX-Polyphemus"
-              >
-                My github Cyclops Polyphemus
-              </a>
-            </nav>
-          </div>
-        </header>
-        <div class="mdl-layout__drawer">
-          <span class="mdl-layout-title">Title</span>
-          <nav class="mdl-navigation">
-            <a
-              class="mdl-navigation__link"
-              href="https://github.com/geoffroygivry/"
-            >
-              My github
-            </a>
-            <a
-              class="mdl-navigation__link"
-              href="https://github.com/geoffroygivry/CyclopsVFX-Unity"
-            >
-              My github Cyclops Unity
-            </a>
-            <a
-              class="mdl-navigation__link"
-              href="https://github.com/geoffroygivry/CyclopsVFX-Arges"
-            >
-              My github Cyclops Arges
-            </a>
-            <a
-              class="mdl-navigation__link"
-              href="https://github.com/geoffroygivry/CyclopsVFX-Polyphemus"
-            >
-              My github Cyclops Polyphemus
-            </a>
-          </nav>
-        </div>
-        <main class="mdl-layout__content">
-          <div class="page-content">fufu!</div>
-        </main>
+      <div class="mdl-layout__header-row">
+        <span class="mdl-layout-title">
+          Gleipnir <small>Asgard</small>
+        </span>
+        <div class="mdl-layout-spacer" />
+        <nav class="mdl-navigation mdl-layout--large-screen-only">
+          <a
+            class="mdl-navigation__link"
+            href="https://github.com/geoffroygivry/"
+          >
+            Github
+          </a>
+          <a
+            class="mdl-navigation__link"
+            href="https://github.com/geoffroygivry/CyclopsVFX-Unity"
+          >
+            Unity
+          </a>
+          <a
+            class="mdl-navigation__link"
+            href="https://github.com/geoffroygivry/CyclopsVFX-Arges"
+          >
+            Arges
+          </a>
+          <a
+            class="mdl-navigation__link"
+            href="https://github.com/geoffroygivry/CyclopsVFX-Polyphemus"
+          >
+            Polyphemus
+          </a>
+        </nav>
       </div>
     );
   }
