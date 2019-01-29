9c13f01d33c723e59e793424efb2693889851c7d
Geoffroy Givry <geoff.givry@gmail.com>
2019-01-29 22:34:50 +0000

    Very Good Start. Added topbar.

---
+++ src/app/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Topbar from "../components/Topbar";
+
+const Asgard = () => {
+  return (
+    <div>
+      <Topbar />
+      <div>Hello World!</div>
+    </div>
+  );
+};
+
+export default Asgard;
