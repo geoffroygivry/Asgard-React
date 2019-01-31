0f5cae992dd4bc3043a911d6081e1b77d5d01a37
Geoffroy Givry <geoff.givry@gmail.com>
2019-01-31 23:41:54 +0000

    work in progress user card

--- src/app/App.js
+++ src/app/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import Topbar from "../components/Topbar";
 import PageContent from "../components/PageContent";
+import TableClients from "../components/TableClients";
+import UserCard from "../components/UserCard";
+import "./App.css";
 
 class Asgard extends React.Component {
   state = {
     mainGrid: "fufu this is 8 col!!!!!!!!!!!!!!!!!!",
     secondaryGrid: "Yep this is a 4 col......................."
   };
+
   render() {
     return (
       <div className="mdl-layout mdl-js-layout">
@@ -14,8 +18,8 @@ class Asgard extends React.Component {
         <main className="mdl-layout__content">
           <div className="page-content">
             <PageContent
-              mainGrid={this.state.mainGrid}
-              secondaryGrid={this.state.secondaryGrid}
+              mainGrid={<TableClients />}
+              secondaryGrid={<UserCard />}
             />
           </div>
         </main>
