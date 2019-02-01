05c618317b66c2d371e6410cafa0a82a7ea2f7ea
geoff.givry <geoff.givry@gmail.com>
2019-02-01 11:55:17 -0500

    added materialUI card

--- src/components/PageContent.js
+++ src/components/PageContent.js
@@ -1,10 +1,13 @@
 import React from "react";
+import CardMaterialUI from "./CardMaterialUI";
 
 const PageContent = props => {
   return (
     <div className="mdl-grid">
       <div className="mdl-cell mdl-cell--8-col">{props.mainGrid}</div>
       <div className="mdl-cell mdl-cell--4-col">{props.secondaryGrid}</div>
+      <div className="mdl-cell mdl-cell--8-col"></div>
+      <div className="mdl-cell mdl-cell--4-col">{<CardMaterialUI />}</div>
     </div>
   );
 };
